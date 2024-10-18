482455a75673771162ec061d41e2fe6854418991
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-18 23:35:11 +0600

    new commit

--- forFirst.js
+++ forFirst.js
@@ -1,3 +1,5 @@
-for (var i = 0; i < 10; i++) {
-    console.log("My name is Hridoy " + (i + 1));
+for (var i = 1; i < 10; i++) {
+    console.log("My name is Hridoy. " + (i++)); //i er value change hoye jabe ar porer line execute howar somoy ta store kore rakhbe
+    console.log("I want to be a software Engineer. " + (i));
+    console.log("I love my family. " + (i = i + 1));
 }
---
+++ forOdd.js
@@ -0,0 +1,5 @@
+for (var i = 0; i < 10; i++) {
+    if (i % 2 == 1) {
+        console.log("This is a ODD number " + (i++));
+    }
+}
