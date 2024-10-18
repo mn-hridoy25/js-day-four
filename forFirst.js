62325212d572f62667338656e4f9131b5971faa1
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-18 23:38:00 +0600

    for added

--- forFirst.js
+++ forFirst.js
@@ -1,4 +1,4 @@
-for (var i = 1; i < 10; i++) {
+for (var i = 1; i < 100; i++) {
     console.log("My name is Hridoy. " + (i++)); //i er value change hoye jabe ar porer line execute howar somoy ta store kore rakhbe
     console.log("I want to be a software Engineer. " + (i));
     console.log("I love my family. " + (i = i + 1));
