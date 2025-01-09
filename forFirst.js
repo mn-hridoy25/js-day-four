17811046ebadb749a7ac18d337fbdbca61ad1817
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2025-01-09 15:58:21 +0600

    new commit

--- forFirst.js
+++ forFirst.js
@@ -1,5 +1,3 @@
-for (var i = 1; i < 100; i++) {
-    console.log("My name is Hridoy. " + (i++)); //i er value change hoye jabe ar porer line execute howar somoy ta store kore rakhbe
-    console.log("I want to be a software Engineer. " + (i));
-    console.log("I love my family. " + (i = i + 1));
+for (let i = 1; i <= 100; i++) {
+    console.log("My name is Mamunur Roshid Hridoy" + " " + i);
 }
