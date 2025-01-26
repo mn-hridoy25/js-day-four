e8939106fb053f98c7a7d937a9d2daa5241a4559
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2025-01-26 12:28:14 +0600

    forMySelf added

--- forFirst.js
+++ forFirst.js
@@ -1,3 +1,4 @@
 for (let i = 1; i <= 100; i++) {
     console.log("My name is Mamunur Roshid Hridoy" + " " + i);
 }
+
---
+++ forMySelf.js
@@ -0,0 +1,10 @@
+var name="Mamunur Roshid Hridoy"
+var institute="Mymensingh Polytechnic Institute"
+var age="20"
+var department="Computer science and technology"
+
+const myself=`My name is ${name}. I am ${age} years old. I am a student. I read in ${institute} department of ${department}.`
+
+for (let i = 0; i < 10; i++) {
+    console.log(myself);
+}
